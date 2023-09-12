8cf6fd95697ede599b2b6cf508eb40a5b865764f
harislee168 <haris.lee168@gmail.com>
2023-09-12 17:54:56 +0800

    inventory service structure

---
+++ inventory-services/src/main/java/com/microservice/example/inventoryservices/InventoryServicesApplication.java
@@ -0,0 +1,12 @@
+package com.microservice.example.inventoryservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServicesApplication {
+
+    public static void main (String [] args) {
+        SpringApplication.run(InventoryServicesApplication.class, args);
+    }
+}
