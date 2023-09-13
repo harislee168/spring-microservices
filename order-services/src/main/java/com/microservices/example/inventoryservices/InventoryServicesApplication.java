8ee0faf7b6824453e6a1a8d085ab92304fc0417d
harislee168 <haris.lee168@gmail.com>
2023-09-13 20:30:57 +0800

    added order-services module

---
+++ order-services/src/main/java/com/microservices/example/inventoryservices/InventoryServicesApplication.java
@@ -0,0 +1,12 @@
+package com.microservices.example.inventoryservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServicesApplication {
+
+    public static void main(String [] args) {
+        SpringApplication.run(InventoryServicesApplication.class, args);
+    }
+}
